3e926457b3b6d0d9385d82b6287e3431c344b503
RyanRothweiler <ryanrothweiler@gmail.com>
2013-04-02 22:18:08 -0700

    new double tapping tutorial

--- source/Assets/scripts/Tutorial Stuff/PinchOutTutorial.js
+++ source/Assets/scripts/Tutorial Stuff/PinchOutTutorial.js
@@ -1,11 +1,16 @@
 #pragma strict
 
 //public vars
+public var SecondTutPos : Vector3; //the position for the second round tutorial
+public var AnywhereTut : GameObject; //the small text saying (ANYWHERE)
+public var RoundOneTwoPos : Vector3;  //the position for the middle round tutorial
 
 //private vars
 private var dragControls : DragControlsPC;
 private var ShowVirgin = true; //if the text has been shown
-private var HideVirgin = true; //if the text has been hiden
+private var RoundTwo = false; //if going to show the second double tap here
+private var RoundOne = true; //if going to show the first double tap here
+private var IntroStop = true; //stop hiding at the intro anim
 
 private var str : String;
 private var i : int;
@@ -25,16 +30,33 @@ function Update ()
 		transform.parent = Camera.main.transform;
 	}
 	
-	//showing
-	if (dragControls.canMoveToPlay && ShowVirgin)
+	//showing round one
+	if (dragControls.canMoveToPlay && ShowVirgin && !RoundTwo && RoundOne)
 	{
-		Type("DOUBLE TAP");
+		print("showing one");
+		Type("DOUBLE TAP HERE");
 		ShowVirgin = false;
+		IntroStop = false;
 	}
-	//hiding
-	if (dragControls.canMoveToWorld)
+	
+	//hiding round one, then show the middle round
+	if (dragControls.canMoveToWorld && RoundOne && !IntroStop && !RoundTwo)
 	{
-		HideVirgin = false;
+		RoundOneHiding();
+	}
+	
+	//showing round two
+	if (dragControls.canMoveToPlay && RoundTwo && ShowVirgin && !RoundOne)
+	{
+		RoundTwoShowing();
+	}
+	
+	//hiding round two
+	if (dragControls.canMoveToWorld && RoundTwo && !IntroStop)
+	{
+		print("hiding two");
+		RoundTwo = false;
+		RoundOne = false;
 		StopAllCoroutines();
 		Type(" ");
 	}
@@ -65,4 +87,36 @@ function Type(text : String) //an effect of typing in something
 		GetComponent(TextMesh).text = str;
 		yield WaitForSeconds(dragControls.TutorialTypeSpeed);
 	}
+}
+
+//hiding for round one and showing round one two
+function RoundOneHiding()
+{
+	print("hiding one");
+	RoundOne = false;
+	RoundTwo = true;
+	ShowVirgin = true;
+	IntroStop = true;
+	
+	yield StartCoroutine(Type(" "));
+	transform.localPosition = RoundOneTwoPos;
+	AnywhereTut.SendMessage("Type","(ANYWHERE)");
+	AnywhereTut.transform.localPosition = Vector3(-2.63, -0.58, 25.66);
+	yield WaitForSeconds(0.2);
+	yield StartCoroutine(Type("NOW DOUBLE TAP AGAIN"));
+}
+
+//show round two
+function RoundTwoShowing()
+{
+	print("showing two");	
+	ShowVirgin = false;
+	IntroStop = false;
+	
+	//hide middle round
+	yield StartCoroutine(Type(" "));
+	AnywhereTut.SendMessage("Type"," ");
+	yield WaitForSeconds(0.2);
+	transform.localPosition = SecondTutPos;
+	Type("DOUBLE TAP HERE");
 }
